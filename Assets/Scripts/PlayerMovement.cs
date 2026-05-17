26dce832118473af1a235a0920cbef85b0c340ce
Arqam Mahboob <bscs24146@itu.edu.pk>
2026-05-17 22:55:55 +0500

    Implemetation:Basic Movements with Animations

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,27 +2,49 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed = 8f;
-    [SerializeField] private float jumpForce = 12f;
+    [SerializeField] private float speed = 8f;
+    [SerializeField] private float jumpForce = 9f;
 
     private Rigidbody2D body;
-    private bool isGrounded;
+    private Animator anim;
+    private bool grounded;
 
     private void Awake()
     {
+        //Grabs references for rigidbody and animator from game object.
         body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     private void Update()
     {
-        float moveInput = Input.GetAxis("Horizontal");
-        body.linearVelocity = new Vector2(moveInput * moveSpeed, body.linearVelocity.y);
+        float horizontalInput = Input.GetAxis("Horizontal");
+        body.linearVelocity = new Vector2(horizontalInput * speed, body.linearVelocity.y);
 
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            body.linearVelocity = new Vector2(body.linearVelocity.x, jumpForce);
-        }
+        //Flip player when facing left/right.
+        if (horizontalInput > 0.01f)
+            transform.localScale = Vector3.one;
+        else if (horizontalInput < -0.01f)
+            transform.localScale = new Vector3(-1, 1, 1);
+
+        if (Input.GetKey(KeyCode.Space) && grounded)
+            Jump();
+
+        //sets animation parameters
+        anim.SetBool("Run", horizontalInput != 0);
+        anim.SetBool("grounded", grounded);
+    }
+    private void Jump()
+    {
+        body.linearVelocity = new Vector2(body.linearVelocity.x, jumpForce);
+        anim.SetTrigger("jump");
+        grounded = false;
     }
 
-   
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+            grounded = true;
+    }
+
 }
